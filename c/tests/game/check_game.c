64b698503147dedad8fe0bb8a340c9263c5ec60b
Manuel Gasser <manuel.gasser.ch@gmail.com>
2022-04-13 23:32:54 +0200

    Added first real test

--- c/tests/game/check_game.c
+++ c/tests/game/check_game.c
@@ -1,3 +1,21 @@
+#include <check.h>
+
+#include "../src/game/game.h"
+
+START_TEST(test_create_game) {
+    /* Arrange */
+    size_t height, width = 100;
+
+    /* Act */
+    game_t* result = create_game(height, width);
+
+    /* Assert */
+    ck_assert_uint_eq(result->height, height);
+    ck_assert_uint_eq(result->width, width);
+
+    free_game(result);
+} END_TEST
+
 int main(void) {
     return 0;
 }
